57396de8efe01b1be129bb3f9c7157e4651b1bbc
Lasse Lautanala <lasselautanala@gmail.com>
2020-11-06 13:38:38 +0200

    Viikko 2 harjoitus

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -17,4 +17,41 @@ public class MaksukorttiTest {
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
     }
+    
+    @Test
+    public void kortinAlkusaldoOikein() {
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
+    
+    @Test
+    public void lataaminenKasvattaaSalsoaOikein() {
+        kortti.lataaRahaa(2510);
+        assertEquals("saldo: 25.20", kortti.toString());
+    }
+    
+    @Test
+    public void saldoVaheneeOikein() {
+        kortti.lataaRahaa(2510);
+        kortti.otaRahaa(1320);
+        assertEquals("saldo: 12.0", kortti.toString());
+    }
+
+    @Test
+    public void saldoEiMuutuJosRahaaEiTarpeeksi() {
+        kortti.lataaRahaa(2510);
+        kortti.otaRahaa(3000);
+        assertEquals("saldo: 25.20", kortti.toString());
+    }
+
+    @Test
+    public void palauttaaTrueJosRahatRiittaa() {
+        kortti.lataaRahaa(2510);
+        assertEquals(true, kortti.otaRahaa(2500));
+    }
+
+    @Test
+    public void palauttaaFalseJosRahatEiRiita() {
+        kortti.lataaRahaa(2510);
+        assertEquals(false, kortti.otaRahaa(2530));
+    }
 }
