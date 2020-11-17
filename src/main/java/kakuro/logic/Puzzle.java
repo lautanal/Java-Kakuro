06e403f73619bf7d4341b6c0df54d8f36b79f764
Lasse Lautanala <lasselautanala@gmail.com>
2020-11-17 17:57:52 +0200

    Puzzle-luokan ohjelman kaunistelua Checkstyleä varten

--- src/main/java/kakuro/logic/Puzzle.java
+++ src/main/java/kakuro/logic/Puzzle.java
@@ -38,7 +38,7 @@ public class Puzzle {
         for (int i = 0; i < this.nRows; i++) {
             for (int j = 0; j < this.nCols; j++) {
                 if (this.map.getChar(i, j) != '#') {
-                    this.squares[i][j] = new Square(this.map.getChar(i, j)-48);
+                    this.squares[i][j] = new Square(this.map.getChar(i, j) - 48);
                     this.map.setChar(i, j, '_');
                 }
             }
@@ -83,13 +83,15 @@ public class Puzzle {
 
     public int setSquare(int i, int j, int number) {
 // Numeron laittaminen ruutuun
-        this.map.setChar(i, j, (char)(number + 48));
+        this.map.setChar(i, j, (char) (number + 48));
         return squares[i][j].setNumber(number);
     }
 
     public boolean checkSquare(int i, int j) {
 // Tarkistetaan, voiko ruutuun laittaa numeron
-        if (i < 1 || i >= this.nRows || j < 1 || j >= this.nCols || this.map.getChar(i, j) == '#') return false;
+        if (i < 1 || i >= this.nRows || j < 1 || j >= this.nCols || this.map.getChar(i, j) == '#') {
+            return false;
+        }
         return true;
     }
     
@@ -98,7 +100,9 @@ public class Puzzle {
         for (int i = 0; i < this.nRows; i++) {
             for (int j = 0; j < this.nCols; j++) {
                 if (this.squares[i][j] != null) {
-                    if (!this.squares[i][j].checkCorrect()) return false;
+                    if (!this.squares[i][j].checkCorrect()) {
+                        return false;
+                    }
                 }
             }
         }
@@ -118,38 +122,52 @@ public class Puzzle {
             for (int j = 0; j < this.nCols; j++) {
                 s = s + this.map.getChar(i, j) + "  ";
             }
-            for (int j=0; j < this.rowList[i].size(); j++) {
+            for (int j = 0; j < this.rowList[i].size(); j++) {
                 s = s + this.rowList[i].get(j).getCorrectSum() + " ";
-                if (this.rowList[i].get(j).getCorrectSum() < 10) s += " ";
+                if (this.rowList[i].get(j).getCorrectSum() < 10) {
+                    s += " ";
+                }
             }
             s += "\n";
         }
         String s0 = "     ";
         String s1 = "     ";
         String s2 = "     ";
-        for (int i = 1; i < this.nCols-1; i++) {
+        for (int i = 1; i < this.nCols - 1; i++) {
             if (this.colList[i].size() == 0) {
                 s0 += "   ";
                 s1 += "   ";
                 s2 += "   ";
             } else if (this.colList[i].size() == 1) {
                 s0 = s0 + this.colList[i].get(0).getCorrectSum() + " ";
-                if (this.colList[i].get(0).getCorrectSum() < 10) s0 += " ";
+                if (this.colList[i].get(0).getCorrectSum() < 10) {
+                    s0 += " ";
+                }
                 s1 += "   ";
                 s2 += "   ";
             } else if (this.colList[i].size() == 2) {
                 s0 = s0 + this.colList[i].get(0).getCorrectSum() + " ";
-                if (this.colList[i].get(0).getCorrectSum() < 10) s0 += " ";
+                if (this.colList[i].get(0).getCorrectSum() < 10) {
+                    s0 += " ";
+                }
                 s1 = s1 + this.colList[i].get(1).getCorrectSum() + " ";
-                if (this.colList[i].get(1).getCorrectSum() < 10) s1 += " ";
+                if (this.colList[i].get(1).getCorrectSum() < 10) {
+                    s1 += " ";
+                }
                 s2 += "   ";
             } else {
                 s0 = s0 + this.colList[i].get(0).getCorrectSum() + " ";
-                if (this.colList[i].get(0).getCorrectSum() < 10) s0 += " ";
+                if (this.colList[i].get(0).getCorrectSum() < 10) {
+                    s0 += " ";
+                }
                 s1 = s1 + this.colList[i].get(1).getCorrectSum() + " ";
-                if (this.colList[i].get(1).getCorrectSum() < 10) s1 += " ";
+                if (this.colList[i].get(1).getCorrectSum() < 10) {
+                    s1 += " ";
+                }
                 s2 = s2 + this.colList[i].get(2).getCorrectSum() + " ";
-                if (this.colList[i].get(2).getCorrectSum() < 10) s2 += " ";
+                if (this.colList[i].get(2).getCorrectSum() < 10) {
+                    s2 += " ";
+                }
             }
         }
         s = s + s0 + "\n" + s1 + "\n" + s2 + "\n";
